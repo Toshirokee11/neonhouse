fee81dda0432072850d3b877cacbab2986c2a077
mr01robot <toshiro.kenny@gmail.com>
2021-08-04 19:44:15 -0500

    Modal borrado :p

---
+++ vistas/layout/modal.php
@@ -0,0 +1,53 @@
+        <input type="checkbox" id="btn-modal">
+        <label for="btn-modal" class="lbl-modal"><i class="fas fa-angle-double-down"></i></label>
+        
+        <div class="modal">
+            <div class="contenedor activo">
+                <label for="btn-modal" class="lbl-modal"><i class="fas fa-angle-double-up"></i></label>
+                <h1>CONTENIDO EXCLUSIVO</h1>
+                <div class="contenido">
+                    <div class="izquierda">
+                        <p>¡LLENA EL FORMULARIO!</p>
+                        <p>Tendrás acceso a una galería inédita.</p>
+                        <p>Obtén un descuento exclusivo</p>
+                    </div>
+                    <div class="derecha">
+                        <form action="" method="POST">
+                            <div class="datos">
+                                <input type="text" name="nombre" placeholder="Nombre" id="nombre">
+                                <input type="tel" name="telf" placeholder="Teléfono" id="telefono">
+                            </div>
+                            <input type="email" name="correo" placeholder="Email" id="email"><br>
+                            <button type="submit" value="Enviar" id="btn-enviar">Enviar</button>
+                            <div class="logos">
+                                <a class="sociales__item" target="_blank" href="https://www.instagram.com/neon_led_store/"><i class="fab fa-instagram-square"></i></a>
+                                <a class="sociales__item" target="_blank" href="https://www.facebook.com/nhldecoracioncomercial"> <i class="fab fa-facebook"></i></a>
+                                <a class="sociales__item"  target="_blank" href="https://www.youtube.com/user/NEONHOUSELEDS"> <i class="fab fa-youtube"></i></a>
+                                <a class="sociales__item" target="_blank" href="https://twitter.com/NEONHOUSELEDS"><i class="fab fa-twitter-square"></i></a>
+                                <a class="sociales__item" target="_blank" href="https://www.linkedin.com/in/neonhouseled/"><i class="fab fa-invision"></i></a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="contenedor inactivo">
+                <label for="btn-modal" class="lbl-modal"><i class="fas fa-angle-double-up"></i></label>
+                <div class="contenido">
+                    <div class="izquierda">
+                        <div id="photosphere"> </div>
+                    </div>
+                    <div class="derecha">
+                        <h3>¡GRACIAS!</h3>
+                        <p class="texto">Código de dscto:</p>
+                        <p class="codigo">2021_XI0TA05DESC00110%</p>
+                        <div class="logos">
+                            <a class="sociales__item" target="_blank" href="https://www.instagram.com/neon_led_store/"><i class="fab fa-instagram-square"></i></a>
+                            <a class="sociales__item" target="_blank" href="https://www.facebook.com/nhldecoracioncomercial"> <i class="fab fa-facebook"></i></a>
+                            <a class="sociales__item"  target="_blank" href="https://www.youtube.com/user/NEONHOUSELEDS"> <i class="fab fa-youtube"></i></a>
+                            <a class="sociales__item" target="_blank" href="https://twitter.com/NEONHOUSELEDS"><i class="fab fa-twitter-square"></i></a>
+                            <a class="sociales__item" target="_blank" href="https://www.linkedin.com/in/neonhouseled/"><i class="fab fa-invision"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
